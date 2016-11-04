3a400b6621d348e50bf6823b2c109317ff66538b
Eric Marberg <eric.marberg@qadium.com>
2016-11-04 14:20:07 +0000

    starter tests

---
+++ tests/test_algebra.py
@@ -0,0 +1,56 @@
+import mock
+import pytest
+
+from ..braids import RationalNumber
+
+
+class TestRationalNumbers:
+
+    def test_constructor(self):
+        RationalNumber(1, 2)
+
+    def test_reduce(self):
+        RationalNumber(1, 2)
+
+    def test_to_rational_number(self):
+        RationalNumber(1, 2)
+
+    def test_is_integer(self):
+        RationalNumber(1, 2)
+
+    def test_hash(self):
+        RationalNumber(1, 2)
+
+    def test_comparisons(self):
+        RationalNumber(1, 2)
+
+    def test_addition(self):
+        RationalNumber(1, 2)
+
+    def test_multiplication(self):
+        RationalNumber(1, 2)
+
+    def test_division(self):
+        RationalNumber(1, 2)
+
+    def test_power(self):
+        RationalNumber(1, 2)
+
+    def test_repr(self):
+        RationalNumber(1, 2)
+
+
+class TestPrimeFactorization:
+    pass
+
+
+class TestQuadraticNumbers:
+    pass
+
+
+class TestMonomial:
+    pass
+
+
+class TestPolynomial:
+    pass
